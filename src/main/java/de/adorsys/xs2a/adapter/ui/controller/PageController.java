b6d388d16780b817396754db5df931fbead6059b
berezkin88 <berezkin05@gmail.com>
2019-12-27 16:09:19 +0200

    remove unused inport

--- src/main/java/de/adorsys/xs2a/adapter/ui/controller/PageController.java
+++ src/main/java/de/adorsys/xs2a/adapter/ui/controller/PageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 import java.util.Map;
 
 @Controller
